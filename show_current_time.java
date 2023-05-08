1306d893e2047c5e941d04c37b8f0d3b7daf646c
Muhammad Ali <89317342+SeAliijaz@users.noreply.github.com>
2023-05-08 22:39:13 +0500

    Create show_current_time.java

---
+++ show_current_time.java
@@ -0,0 +1,12 @@
+
+import java.time.LocalTime;
+
+public class GetCurrentTime {
+    public static void main(String[] args) {
+        // Get the current time
+        LocalTime currentTime = LocalTime.now();
+        
+        // Print the current time to the console
+        System.out.println("Current time: " + currentTime);
+    }
+}
